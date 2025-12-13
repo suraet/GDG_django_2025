3b6e06780e26aa2d5385321ef0613b00f2b9942a
Your Name <you@example.com>
2025-12-13 12:15:03 +0300

    week 3

--- week 1/exercise3.py
+++ week 1/exercise3.py
@@ -1,18 +1,23 @@
 a = input ( "enter the log data ")
-# try :
-#     with open("log.txt","r") as f:
-#        p =  f.read()
-#        print(p)
-# except:
-#     with open("log.txt","w") as m:
-#         m.write(a)
-#     with open("logtxt","r") as f:
-#        p =  f.read()
-#        print(p)
-with open("log.txt","w") as m:
-        m.write(a)
-with open("log.txt","r") as f:
+def log():
+    try :
+      with open("log.txt","a") as f:
+       p =  f.write(a +"\n")
+       print(p)
+      with open("log.txt","r") as f:
        p =  f.read()
        print(p)
+    except:
+     with open("log.txt","w") as m:
+        m.write(a)
+     with open("logtxt","r") as f:
+       p =  f.read()
+       print(p)
+log()
+# with open("log.txt","w") as m:
+#         m.write(a)
+# with open("log.txt","r") as f:
+#        p =  f.read()
+#        print(p)
 
 
--- week 1/shop.py
+++ week 1/shop.py
@@ -1,5 +1,9 @@
+#a = int (input ("how many item u want to buy"))
+def discount(a):
+    if a <= 3:
+     print("sry no discount applied")
+    else :
+     print("discount applied ")
+
 a = int (input ("how many item u want to buy"))
-if a <= 3:
-    print("sry no discount applied")
-else :
-    print("discount applied ")
+discount(a)
